7730d3e66d20c22fa8ae3be252244ef2dec57286
Swapnil Kumar Sahu <78612493+swapnil-092@users.noreply.github.com>
2023-09-11 15:05:28 +0530

    Created Django App and Models for Product

--- Ecommerce API/ecommerce/ecommerce/urls.py
+++ Ecommerce API/ecommerce/ecommerce/urls.py
@@ -1,5 +1,5 @@
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -9,4 +9,4 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls.authtoken')),
     # path('api/v1/', include('product.urls')),
     # path('api/v1/', include('order.urls')),
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
